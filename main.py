b087b93d84ff611ab289546bea2050980e4bceb5
cbigong <christianmiche.bigong@epfedu.fr>
2022-12-06 12:12:30 +0100

    second_commit

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=345174772"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
+</script>
+ """
+ return prefix_google + "Hello World"
 from flask import Flask
 
 app = Flask(__name__)
